45dd1c4f40b77c4a2bc25b1225cb7483421c6e05
pulu <pulu@pinduoduo.com>
2020-07-27 21:24:29 +0800

    Initial Commit

---
+++ HideText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HideText
+//
+//  Created by Zhou Wei Ran on 2020/7/27.
+//  Copyright © 2020 Paper Scratch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
